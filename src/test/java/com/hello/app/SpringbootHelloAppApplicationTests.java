aed32b43ecd73225dde7c7a9a31acc4503c50856
Sharad pareek <80637947+sharad0111@users.noreply.github.com>
2025-03-05 16:17:26 +0530

    sqs listner

---
+++ src/test/java/com/hello/app/SpringbootHelloAppApplicationTests.java
@@ -0,0 +1,12 @@
+//package com.hello.app;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootHelloAppApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
